fd8206a78958f5d1219201ce54bc4278809f55ab
Mostafa Sakr <mostafa@tradecloud.nl>
2017-02-21 15:07:26 +0000

    Project Init

---
+++ TestJenkisBuild/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestJenkisBuild.Startup))]
+namespace TestJenkisBuild
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
